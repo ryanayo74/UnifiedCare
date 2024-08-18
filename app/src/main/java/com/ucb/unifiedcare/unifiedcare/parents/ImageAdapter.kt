814068e666b6edba1669292f2717dd388d9a99c6
Kershey Mae Heredia <kersheyheredia20@gmail.com>
2024-08-18 17:54:45 +0800

    adding parents facility inforamtion, facilitylis, facility list item

---
+++ app/src/main/java/com/ucb/unifiedcare/unifiedcare/parents/ImageAdapter.kt
@@ -0,0 +1,31 @@
+package com.ucb.unifiedcare.unifiedcare.parents
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.ucb.unifiedcare.R
+
+class ImageAdapter (private val imageList: List<Int>) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.image, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.itemView.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+        holder.itemView.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+        holder.imageView.setImageResource(imageList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+}
---
+++ app/src/main/java/com/ucb/unifiedcare/unifiedcare/parents/ParentsFacilityListActivity.kt
@@ -0,0 +1,12 @@
+package com.ucb.unifiedcare.unifiedcare.parents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ucb.unifiedcare.R
+
+class ParentsFacilityListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parents_facility_list)
+    }
+}
