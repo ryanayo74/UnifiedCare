6692ebd3856993b89855812db11317fef8d15235
Kershey Mae Heredia <kersheyheredia20@gmail.com>
2024-09-03 00:43:18 +0800

    progress summary, reco page, skills assess, therapust notes, message page(parent)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,24 +37,21 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:23.0.0")
-    implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
-    implementation ("com.google.android.libraries.places:places:3.1.0")
-    implementation("androidx.activity:activity:1.9.1")
+    implementation("com.google.firebase:firebase-firestore-ktx:25.1.0")
+    implementation ("com.google.android.libraries.places:places:3.5.0")
+    implementation("androidx.activity:activity-ktx:1.9.1")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
-    implementation ("com.google.android.material:material:1.9.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation ("androidx.viewpager2:viewpager2:1.1.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation ("com.jakewharton.threetenabp:threetenabp:1.4.4")
-
-
-
+//  implementation ("com.github.gastricspark:scrolldatepicker:0.0.1")
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
